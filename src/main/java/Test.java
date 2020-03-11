f2d947c50781f11f8829780b6d5de80d5b76065b
Oh Dong-geon <donggeon.dev@gmail.com>
2020-03-11 19:54:42 +0900

    initial commit

---
+++ src/main/java/Test.java
@@ -0,0 +1,11 @@
+/**
+ * Created by donggeon on 2020/03/11
+ * Dankook UNIV. Computer Science
+ * Source :
+ * Desc :
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("hello world!");
+    }
+}
