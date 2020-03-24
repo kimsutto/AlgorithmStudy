32b7892e55172d60864ebcc0993a90583ea3bcfd
LeeDonggun <tnpdlzi@naver.com>
2020-03-24 16:20:44 +0900

    [ADD] BOJ 'no.10809' by.이동건

---
+++ code/BOJ/no.10809/no.10809_이동건.py
@@ -0,0 +1,3 @@
+s = input()
+
+print(s.find('a'), s.find('b'), s.find('c'), s.find('d'), s.find('e'), s.find('f'), s.find('g'), s.find('h'), s.find('i'), s.find('j'), s.find('k'), s.find('l'), s.find('m'), s.find('n'), s.find('o'), s.find('p'), s.find('q'), s.find('r'), s.find('s'), s.find('t'), s.find('u'), s.find('v'), s.find('w'), s.find('x'), s.find('y'), s.find('z'))
