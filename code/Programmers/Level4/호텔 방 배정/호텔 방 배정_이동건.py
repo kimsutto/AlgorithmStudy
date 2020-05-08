4e327fa38a639eafcac05ab99d89aa80c9783789
LeeDonggun <tnpdlzi@naver.com>
2020-05-08 14:41:23 +0900

    [MOD] Programmers 'Lv4.호텔 방 배정' by.이동건

--- code/Programmers/Level4/호텔 방 배정/호텔 방 배정_이동건.py
+++ code/Programmers/Level4/호텔 방 배정/호텔 방 배정_이동건.py
@@ -1,17 +1,18 @@
 k = 40
 room_number = [1, 5, 4, 2, 3, 5, 4,3, 2, 1, 5, 3, 24, 15, 16, 4, 3, 2,6, 7, 8, 11, 21, 11, 14, 14, 15 ]
 
+def sortque(dict, i):
+    if (i not in dict.keys()):
+        dict[i] = 1
+    else:
+        v = dict[i]
+        dict[i] += 1
+        sortque(dict, i+v)
 
 def solution(k, room_number):
     dict = {}
     for i in room_number:
-        if(i not in dict.keys()):
-            dict[i] = i
-        else:
-            while(i in dict.keys()):
-                i += 1
-
-            dict[i] = i
+        sortque(dict, i)
 
     answer = []
     keys = dict.keys()
