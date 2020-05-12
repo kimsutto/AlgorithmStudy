2d163c205c48c44d5611f5178e4ceed35de9b977
LeeDonggun <tnpdlzi@naver.com>
2020-05-12 17:05:49 +0900

    [ADD] BOJ 'no.15650' by.이동건

---
+++ code/BOJ/no.15650/no.15650_이동건.py
@@ -0,0 +1,26 @@
+n, m = map(int, input().split())
+
+numlist = [i+1 for i in range(n)]
+checklist = [False] * n
+
+arr = []
+
+def dfs(cnt):
+    if(cnt == m):
+        print(arr)
+        return
+    for i in range(n):
+        if(checklist[i]):
+            continue
+
+        checklist[i] = True
+        arr.append(numlist[i])
+        dfs(cnt+1)
+        arr.pop()
+
+
+        for j in range(i + 1, n):
+            checklist[j] = False
+
+
+dfs(0)
