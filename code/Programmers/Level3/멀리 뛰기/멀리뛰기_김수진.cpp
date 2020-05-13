f6958c457bb3ceb5a0a9c45f50436d809f7d591e
kimsutto <suginkim1@gmail.com>
2020-05-13 15:53:37 +0900

    Programmers 'Lv.3 멀리뛰기'by.김수진

---
+++ code/Programmers/Level3/멀리 뛰기/멀리뛰기_김수진.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+long long solution(int n) {
+    long long answer = 0;
+    long dp[2000];
+    dp[1]=1;
+    dp[2]=2;
+    
+    for(int i=3;i<=n;i++){
+        dp[i]=(dp[i-1]+dp[i-2])%1234567;
+    }
+    cout<<dp[n];
+    return dp[n];
+}
