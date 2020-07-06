8e3dd012ce8153680cefcbc24826730e43f7fd46
leeminjung1 <52394879+leeminjung1@users.noreply.github.com>
2020-07-06 20:55:09 +0900

    [ADD] BOJ 'no.2447' by.이민정

---
+++ code/BOJ/no.2447_이민정.c
@@ -0,0 +1,27 @@
+no.2447/
+#include <stdio.h>
+
+void draw(int x, int y) {
+	while (y != 0) {
+		if (y % 3 == 1 && x % 3 == 1){
+			printf(" ");
+			return 0;
+		}
+		y /= 3;
+		x /= 3;
+	}
+	printf("*");
+}
+int main() {
+	int n;
+	scanf("%d",&n);
+	
+	for (int y = 0; y < n; y++) {
+		for (int x = 0; x < n; x++) {
+			draw(x, y);
+		}
+		printf("\n");
+	}
+
+	return 0;
+}
