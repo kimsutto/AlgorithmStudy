8767afa064a2b2f07623fe2a04f87b2e2b9563ea
leeminjung1 <52394879+leeminjung1@users.noreply.github.com>
2020-07-10 00:15:30 +0900

    [MOD] Programmers 'Lv1.짝수와 홀수' by.이민정

---
+++ code/Programmers/Level1/짝수와 홀수/짝수와 홀수_이민정.cpp
@@ -0,0 +1,11 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(int num) {
+    string answer = "";
+    
+    answer = num%2 ? "Odd" : "Even";
+    return answer;
+}
