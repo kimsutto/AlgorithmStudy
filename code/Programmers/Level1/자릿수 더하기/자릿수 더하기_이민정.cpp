a8890ff57b7d79c397dfa9dcdd0857fe8328a8a7
leeminjung1 <52394879+leeminjung1@users.noreply.github.com>
2020-07-11 03:17:54 +0900

    [ADD] Programmers 'Lv1. 자릿수 더하기' by.이민정

---
+++ code/Programmers/Level1/자릿수 더하기/자릿수 더하기_이민정.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+int solution(int n)
+{
+    int answer = 0;
+
+    while(n>=1){
+        answer += (n%10);
+        n = n/10;
+    }
+    
+    return answer;
+}
