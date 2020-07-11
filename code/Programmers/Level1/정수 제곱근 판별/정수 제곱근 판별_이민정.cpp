539e4179bf5138ece4c07ac29d55c8380e40f216
leeminjung1 <52394879+leeminjung1@users.noreply.github.com>
2020-07-11 23:30:04 +0900

    [MOD] Programmers 'Lv1. 정수 제곱근 판별' by.이민정

--- code/Programmers/Level1/정수 제곱근 판별/정수 제곱근 판별_이민정.cpp
+++ code/Programmers/Level1/정수 제곱근 판별/정수 제곱근 판별_이민정.cpp
@@ -1,12 +1,16 @@
 #include <string>
 #include <vector>
-#include <math.h>
-
 using namespace std;
 
 long long solution(long long n) {
     long long answer = -1;
-    if((int)sqrt(n)==sqrt(n))
-        answer = (sqrt(n)+1)*(sqrt(n)+1);
+    
+    for(long long i=1;i<=n;i++){
+        if(n/i==i && n%i==0){
+            answer = (i+1)*(i+1);
+            break;
+        }
+    }
+    
     return answer;
 }
