2dcfd918cd9f50e4d8845bb53d6f67bb2ef1b87d
leeminjung1 <52394879+leeminjung1@users.noreply.github.com>
2020-07-14 23:08:19 +0900

    [ADD] Programmers 'Lv1. 문자열 내 p와 y의 개수' by.이민정

---
+++ code/Programmers/Level1/문자열 내 p와 y의 개수/문자열 내 p와 y의 개수_이민정.cpp
@@ -0,0 +1,19 @@
+#include <string>
+#include <iostream>
+using namespace std;
+
+bool solution(string s)
+{
+    int p=0;
+    int y=0;
+    
+    for(int i=0; i<s.size(); i++){
+        if(s[i]=='p'||s[i]=='P')
+            p++;
+        else if(s[i]=='y'||s[i]=='Y')
+            y++;
+    }
+    
+    return p==y ? true : false;
+
+}
