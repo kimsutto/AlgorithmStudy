55fefb25688555809f22bb13c20f66e04c375cc7
leeminjung1 <52394879+leeminjung1@users.noreply.github.com>
2020-08-03 15:56:58 +0900

    [ADD] Programmers 'Lv1.124 나라의 숫자' by.이민정

---
+++ code/Programmers/Level2/124 나라의 숫자/124 나라의 숫자_이민정.cpp
@@ -0,0 +1,24 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+string solution(int n) {
+    string answer = "";
+    
+    while(1){   //10%3==1, 10/3=3, 3%3==0, 3/3=1
+        
+        if(n%3==1)answer.push_back('1');
+        else if(n%3==2) answer.push_back('2');
+        else {
+            answer.push_back('4');
+            n--;
+        }
+        n = n/3;
+        if(n<1) break;
+    }
+    
+    reverse(answer.begin(), answer.end());
+    
+    return answer;
+}
