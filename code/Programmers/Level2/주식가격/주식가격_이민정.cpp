4b6770f42a61b45abdb8cb1b5e1d31994b1b0f9a
leeminjung1 <52394879+leeminjung1@users.noreply.github.com>
2020-08-07 16:53:37 +0900

    [ADD] Programmers 'Lv2.주식가격' by.이민정

---
+++ code/Programmers/Level2/주식가격/주식가격_이민정.cpp
@@ -0,0 +1,20 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(vector<int> prices) {
+    vector<int> answer;
+    int count = 0;
+    
+    for(int i=0; i<prices.size(); i++){
+        for(int j=i+1; j<prices.size(); j++){
+            count++;
+            if(prices[i]>prices[j]) break;
+        }
+        answer.push_back(count);
+        count = 0;
+    }
+    
+    return answer;
+}
