3a8c1b028475a0bb71996c92f85355507def5c09
gudwnsgur <gudwnsgur12@naver.com>
2020-09-05 23:10:29 +0900

    [ADD] Programmers 'Lv1.이상한 문자 만들기' by.형준혁

---
+++ code/Programmers/Level1/이상한 문자 만들기/이상한 문자 만들기_형준혁.cpp
@@ -0,0 +1,14 @@
+#include <string>
+#include <vector>
+using namespace std;
+
+string solution(string s) {
+	string answer = "";
+	answer += toupper(s[0]);
+
+	for (int i = 1, wordIndex = 1; i < s.size(); i++, wordIndex++) {
+		if (s[i - 1] == ' ') wordIndex = 0;
+		answer += (wordIndex % 2 ? tolower(s[i]) : toupper(s[i]) );
+	}
+	return answer;
+}
