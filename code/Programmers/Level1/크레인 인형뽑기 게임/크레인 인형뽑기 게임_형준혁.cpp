15b4db10f7dc55436e874362c83ecc9873fe9790
gudwnsgur <gudwnsgur12@naver.com>
2020-09-05 23:38:47 +0900

    [MOD] Programmers 'Lv1.크레인 인형뽑기 게임' by.형준혁

--- code/Programmers/Level1/크레인 인형뽑기 게임/크레인 인형뽑기 게임_형준혁.cpp
+++ code/Programmers/Level1/크레인 인형뽑기 게임/크레인 인형뽑기 게임_형준혁.cpp
@@ -1,44 +1,29 @@
 #include <string>
-#include <vector>
-#include <iostream>
 #include <stack>
+#include <vector>
 using namespace std;
 
-int solution(vector<vector<int>> board, vector<int> moves)
-{
-    int answer = 0, i = 0;
-    stack<int> s;
+int board[30][30];
+stack<int> s;
 
-    for (i = 0; i < 5 && board[i][moves[0] - 1] == 0; i++);
-    cout << i << endl;
-    if (i < board.size()) {
-        s.push(board[i][moves[0] - 1]);
-        board[i][moves[0] - 1] = 0;
-    }
-
-    for (i = 1; i < moves.size(); i++) {
-        int j = 0;
-        for (; j < board.size() && board[j][moves[i] - 1] == 0; j++);
-        if (j < board.size()) {
-            if (!s.empty() && s.top() == board[j][moves[i] - 1]) {
-                s.pop();
-                answer += 2;
-            }
-            else {
-                s.push(board[j][moves[i] - 1]);
-            }
-            board[j][moves[i] - 1] = 0;
-        }
-
-    }
-
-    return answer;
-}
-
-int main() {
-    vector<vector<int>> board = { {0,0,0,0,0},{0,0,1,0,3}, {0,2,5,0,1}, {4,2,4,4,2}, {3,5,1,3,1} };
-    vector<int> moves = { 1,5,3,5,1,2,1,4 };
-
-    cout << endl << solution(board, moves) << endl;
-    return 0;
+int solution(vector<vector<int>> board, vector<int> moves) {
+	int answer = 0;
+	for (int i = 0; i < moves.size(); i++) {
+		for (int j = 0; j < board.size(); j++) {
+			int cur = board[j][moves[i] - 1];
+			if (cur) {
+				if(!s.empty()) {
+					if (s.top() == cur) {
+						s.pop();
+						answer += 2;
+					}
+					else s.push(cur);
+				}
+				else s.push(cur);
+				board[j][moves[i] - 1] = 0;
+				break;
+			}
+		}
+	}
+	return answer;
 }
