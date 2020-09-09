85841ad79be29dfb10b82285447be33838f12afb
kimsutto <57741166+kimsutto@users.noreply.github.com>
2020-09-09 11:56:46 +0900

    [ADD] Programmers 'Lv1.문자열 다루기 기본' by.김수진

---
+++ code/Programmers/Level1/문자열 다루기 기본/문자열 다루기 기본_김수진.cpp
@@ -0,0 +1,16 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+bool solution(string s) {
+    bool answer = true;
+    if(s.size()!=4 && s.size()!=6){
+        return false;
+    }
+    for(int i=0; i<s.size(); i++){
+        if(!(s[i]>='0'&&s[i]<='9'))
+            return false;
+    }
+    return answer;
+}
