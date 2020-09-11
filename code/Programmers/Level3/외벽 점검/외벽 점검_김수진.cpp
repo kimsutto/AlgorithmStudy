0e6b0af6ae6f7048cbbd0f9e3eab0ce0a85d2e12
kimsutto <57741166+kimsutto@users.noreply.github.com>
2020-09-12 01:01:15 +0900

    [MOD] Programmers 'Lv3.외벽 점검'by.김수진

--- code/Programmers/Level3/외벽 점검/외벽 점검_김수진.cpp
+++ code/Programmers/Level3/외벽 점검/외벽 점검_김수진.cpp
@@ -8,51 +8,50 @@ int chkWeak = 0;
 void dfs(int start, deque<int> dq, vector<int> dist, int num){
     result++;
     int end = dq[start] + dist[num];
-    
-    int i = 0;
     chkWeak++;
-    
-    while(dq[start+i+1]<=end){
-        i++;
+    int i = start+1;
+    for(; i<dq.size(); i++ ){
+        if(dq[i] > end)
+            break;
         chkWeak++;
-        if(start+i+1 == dq.size()-1){
-            return;
-        }
-        
     }
-    
-    if(chkWeak >= dq.size()){
+    if(chkWeak>=dq.size()){
         return;
     }
-    
-    
     if(num==dist.size()-1){
-        result = 10;
+        result = 10000;
+        return;
+    }else{
+        dfs(i, dq ,dist, num+1);
         return;
     }
-    dfs(start+i, dq ,dist, num+1);
 }
 
 int solution(int n, vector<int> weak, vector<int> dist) {
-    int answer = 10;
-    sort(dist.begin(), dist.end(),greater<int>());
-    
+    int answer = 10000;
     deque<int> dq;
     for(int i=0; i<weak.size() ; i++){
         dq.push_back(weak[i]);
     }
     
-    for(int i = 0; i < dq.size(); i++){
-        result = 0;
-        chkWeak = 0;
-        dfs(0,dq,dist,0);
-        answer = min(answer, result);
-        int a = dq.front();
-        a = a+n-1;
-        dq.push_back(a);
-        dq.pop_front();
-    }
-    if(answer==10)  return -1;
+    
+
+    
+    do{
+        for(int i = 0; i < dq.size(); i++){
+            result = 0;
+            chkWeak = 0;
+            dfs(0,dq,dist,0);
+        
+            answer = min(answer, result);
+            int a = dq.front();
+            a = a+n;
+            dq.push_back(a);
+            dq.pop_front();
+        }
+    }while(next_permutation(dist.begin(),dist.end()));
+        
+    if(answer>=10)  return -1;
     
     return answer;
 }
