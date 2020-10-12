5ce93e1fdb04ac7281eb1e20f7d895af6c077c43
Donggun <tnpdlzi@naver.com>
2020-10-12 20:01:06 +0900

    [ADD] Programmers 'Lv.2 가장 큰 수' by.이동건

---
+++ code/Programmers/Level2/가장 큰 수/가장 큰 수_이동건.py
@@ -0,0 +1,8 @@
+numbers = [6, 10, 2]
+
+def solution(numbers):
+    numbers = list(map(str, numbers))
+    numbers.sort(key=lambda x: x * 3, reverse=True)
+    return str(int(''.join(numbers)))
+
+print(solution(numbers))
