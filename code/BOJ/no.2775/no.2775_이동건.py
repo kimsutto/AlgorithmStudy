7ce9f9a2a918318474b0d7e714dd029085bbe2ac
이동건 <tnpdlzi@naver.com>
2020-10-16 18:56:37 +0900

    [ADD] BOJ 'no.2775' by.이동건

---
+++ code/BOJ/no.2775/no.2775_이동건.py
@@ -0,0 +1,12 @@
+T = int(input())
+
+for _ in range(T):
+    k = int(input())
+    n = int(input())
+    people = [ i for i in range(1, n+1)]
+
+    for __ in range(k):
+        for j in range(1,n):
+            people[j] += people[j-1]
+    print(people[-1])
+
