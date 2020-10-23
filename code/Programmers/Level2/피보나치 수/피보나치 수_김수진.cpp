7d67d3f7f02875c5866baeee1dff8009a13a0e8e
kimsutto <57741166+kimsutto@users.noreply.github.com>
2020-10-23 16:18:11 +0900

    [ADD] Programmers 'Lv2.피보나치 수' by.김수진

---
+++ code/Programmers/Level2/피보나치 수/피보나치 수_김수진.cpp
@@ -0,0 +1,15 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int n) {
+    int answer = 0;
+    int dp[100001];
+    dp[0] = 0; 
+    dp[1] = 1;
+    for(int i=2; i<=n; i++){
+        dp[i] = (dp[i-2] + dp[i-1]) %1234567;
+    }
+    return dp[n];
+}
